e3b795587b4bb5d15f7174d4dcd4c6f599be5dfb
Andy Lian <andylke@gmail.com>
2022-02-22 21:39:51 +0800

    Fix EchoRestController, add RequestMapping path.

--- src/main/java/io/github/andylke/demo/echo/EchoRestController.java
+++ src/main/java/io/github/andylke/demo/echo/EchoRestController.java
@@ -3,10 +3,12 @@ package io.github.andylke.demo.echo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/echo")
 public class EchoRestController {
 
   @GetMapping
